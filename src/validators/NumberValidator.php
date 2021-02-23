990b3fc997bb4141d0885f0c9ce88e7c5ec598b8
solar05 <artem00298@gmail.com>
2021-02-23 03:31:35 +0300

    Refactor number validator

--- src/validators/NumberValidator.php
+++ src/validators/NumberValidator.php
@@ -4,60 +4,40 @@ namespace Hexlet\Validator\Validators;
 
 class NumberValidator
 {
-    protected int $number = 0;
-    protected bool $required = false;
-    protected bool $positive = false;
-    protected mixed $range = [];
     protected mixed $customValidations = [];
-    protected mixed $activatedValidations = [];
+    protected mixed $validations = [];
 
     public function __construct(mixed $validations = [])
     {
+        $this->validations['default'] = fn($number) => $number == null || is_integer($number);
         $this->customValidations = $validations;
     }
 
     public function isValid(mixed $number): bool
     {
-        $rangeSet = $this->range != [];
-        if (!$this->required && $number == null) {
-            return true;
-        }
-        if ($this->required && !is_integer($number)) {
-            return false;
-        }
-        if ($this->positive) {
-            return $rangeSet ? ($this->inRange($number) && $number > 0) : $number > 0;
-        }
-        if ($rangeSet) {
-            return $this->inRange($number);
-        }
-        if (count($this->activatedValidations) > 0) {
-            $result = [];
-            foreach ($this->activatedValidations as $validation) {
-                if (!$validation($number)) {
-                    $result[] = false;
-                }
+        foreach ($this->validations as $validation) {
+            if (!$validation($number)) {
+                return false;
             }
-            return count($result) === 0;
         }
         return true;
     }
 
     public function required(): NumberValidator
     {
-        $this->required = true;
+        $this->validations['required'] = fn($number) => is_integer($number);
         return $this;
     }
 
     public function range(int $min, int $max): NumberValidator
     {
-        $this->range = ['min' => $min, 'max' => $max];
+        $this->validations['range'] = fn($number) => !($number < $min || $number > $max);
         return $this;
     }
 
     public function positive(): NumberValidator
     {
-        $this->positive = true;
+        $this->validations['positive'] = fn($number) => $number >= 0;
         return $this;
     }
 
@@ -65,15 +45,8 @@ class NumberValidator
     {
         if (array_key_exists($name, $this->customValidations)) {
             $fn = $this->customValidations[$name];
-            $this->activatedValidations[$name] = fn($num) => $fn($num, $value);
+            $this->validations[$name] = fn($num) => $fn($num, $value);
         }
         return $this;
-    }
-
-    private function inRange(int $number): bool
-    {
-        $min = $this->range['min'];
-        $max = $this->range['max'];
-        return !($number < $min || $number > $max);
     }
 }
