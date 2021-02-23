73b8523d3c2666a524358ffa018532ec8cb6626b
solar05 <artem00298@gmail.com>
2021-02-23 03:21:46 +0300

    Refactor string validator

--- src/validators/StringValidator.php
+++ src/validators/StringValidator.php
@@ -4,49 +4,34 @@ namespace Hexlet\Validator\Validators;
 
 class StringValidator
 {
-    protected string $str = "";
-    protected bool $required = false;
-    protected int $length = 0;
     protected mixed $customValidations = [];
-    protected mixed $activatedValidations = [];
+    protected mixed $validations = [];
 
     public function __construct(mixed $validations = [])
     {
+        $this->validations['default'] = fn($str) => is_string($str);
         $this->customValidations = $validations;
     }
 
-    public function contains(string $str = ''): StringValidator
+    public function contains(string $string = ''): StringValidator
     {
-        $this->str = $str;
+        $this->validations['contains'] = fn($str) => str_contains($str, $string);
         return $this;
     }
 
     public function isValid(mixed $str): bool
     {
-        if (!is_string($str)) {
-            return false;
-        }
-        if ($this->required) {
-            return $str != '' ? str_contains($str, $this->str) : false;
-        }
-        if ($this->length != 0) {
-            return strlen($str) >= $this->length;
-        }
-        if (count($this->activatedValidations) > 0) {
-            $result = [];
-            foreach ($this->activatedValidations as $validation) {
-                if (!$validation($str)) {
-                    $result[] = false;
-                }
+        foreach ($this->validations as $validation) {
+            if (!$validation($str)) {
+                return false;
             }
-            return count($result) === 0;
         }
-        return str_contains($str, $this->str);
+        return true;
     }
 
     public function required(): StringValidator
     {
-        $this->required = true;
+        $this->validations['required'] = fn($str) => is_string($str) && $str !== '';
         return $this;
     }
 
@@ -54,14 +39,14 @@ class StringValidator
     {
         if (array_key_exists($name, $this->customValidations)) {
             $fn = $this->customValidations[$name];
-            $this->activatedValidations[$name] = fn($string) => $fn($string, $value);
+            $this->validations[$name] = fn($string) => $fn($string, $value);
         }
         return $this;
     }
 
     public function minLength(int $length): StringValidator
     {
-        $this->length = $length;
+        $this->validations['minLength'] = fn($str) => strlen($str) >= $length;
         return $this;
     }
 }
